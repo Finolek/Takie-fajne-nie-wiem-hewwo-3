4c57b7d801fdfd2034c64c1c485e6b9b8c987f99
Finolek <130661375+Finolek@users.noreply.github.com>
2023-04-13 13:26:48 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp4;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(1, -3, 4);
+
+            try
+            {
+                double[] rozwiazania = rownanie.ObliczRozwiazanie();
+                Console.WriteLine("Rozwiązania: x1 = {0}, x2 = {1}", rozwiazania[0], rozwiazania[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Rownanie kwadratowe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class RownanieKwadratowe
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public RownanieKwadratowe(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double ObliczDelte()
+        {
+            return b * b - 4 * a * c;
+        }
+
+        public double[] ObliczRozwiazanie()
+        {
+            double delta = ObliczDelte();
+
+            if (delta < 0)
+            {
+                throw new Exception("Delta jest ujemna");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                return new double[] { x, x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+    }
+}
